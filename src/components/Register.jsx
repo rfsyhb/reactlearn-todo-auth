64af6f4fe376f5d443c611629d71cefd70907c41
rfsyhb <rafisyihab3@gmail.com>
2024-07-01 22:48:00 +0700

    add Register page

---
+++ src/components/Register.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { registerUser } from '../slices/userSlice';
+
+/**
+ * registerUser(userData)
+ * name, email, password
+ */
+export default function Register() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const error = useSelector((state) => state.users.error);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const resultAction = await dispatch(
+      registerUser({ name, email, password })
+    );
+    if (registerUser.fulfilled.match(resultAction)) {
+      navigate('/login');
+    }
+  };
+
+  return (
+    <div>
+      <h2>Register</h2>
+      {error && <div>Error: {error}</div>}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+}
