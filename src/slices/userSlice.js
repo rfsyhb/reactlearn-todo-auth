bb1556135fb11d394c352c7616c43f9a6e29374f
rfsyhb <rafisyihab3@gmail.com>
2024-07-01 16:06:06 +0700

    create userSlice: asyncThunk, action, async action

---
+++ src/slices/userSlice.js
@@ -0,0 +1,95 @@
+/* eslint-disable no-param-reassign */
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import apiRequest from '../service/api';
+
+/**
+ * 01 - Thunks
+ * registerUser(userData), loginUser(credentials), fetchUsers()
+ */
+export const registerUser = createAsyncThunk(
+  'user/register',
+  async (userData) => {
+    const response = await apiRequest('post', '/users/register', userData);
+    return response.data.data.newUser;
+  }
+);
+
+export const loginUser = createAsyncThunk('user/login', async (credentials) => {
+  const response = await apiRequest('post', '/users/login', credentials);
+  return response.data.token;
+});
+
+export const fetchUsers = createAsyncThunk('user/fetchUsers', async () => {
+  const response = await apiRequest('get', '/users');
+  return response.data.data.users;
+});
+
+/**
+ * 02 - intialState
+ */
+const initialState = {
+  user: null,
+  token: localStorage.getItem('token') || null,
+  users: [],
+  status: 'idle',
+  error: null,
+};
+
+/**
+ * 03 - slice
+ */
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    // logout -> hapus data authed User
+    logout: (state) => {
+      state.token = null;
+      state.user = null;
+      localStorage.removeItem('token');
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(loginUser.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.token = action.payload;
+      })
+      .addCase(loginUser.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      })
+      .addCase(registerUser.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(registerUser.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.users.push(action.payload);
+      })
+      .addCase(registerUser.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      })
+      .addCase(fetchUsers.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchUsers.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.users = action.payload;
+      })
+      .addCase(fetchUsers.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      });
+  },
+});
+
+/**
+ * 04 - export
+ * action dan reducer
+ */
+export const { logout } = userSlice.actions;
+export default userSlice.reducer;
