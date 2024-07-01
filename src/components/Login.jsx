3a8a067e4687fb0ebbf453198013e0011b82e954
rfsyhb <rafisyihab3@gmail.com>
2024-07-01 22:47:52 +0700

    add Login page

---
+++ src/components/Login.jsx
@@ -0,0 +1,45 @@
+/* eslint-disable no-console */
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { loginUser } from '../slices/userSlice';
+
+export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const error = useSelector((state) => state.users.error);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const resultAction = await dispatch(loginUser({ email, password }));
+    if (loginUser.fulfilled.match(resultAction)) {
+      navigate('/todos');
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      {error && <div>Error: {error}</div>}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
